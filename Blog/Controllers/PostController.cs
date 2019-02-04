cd32d76dc2008d31cfd8f0e408a6a9f27fc532d9
Alexandre Castro <alexandre.castro@outlook.com>
2019-02-04 20:38:41 -0200

    Add post structure

---
+++ Blog/Controllers/PostController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class PostController : Controller
+    {
+        public IActionResult Index()
+        {
+            var lista = new List<Models.Post>
+            {
+                new Models.Post
+                {
+                    Titulo = "Harry Potter",
+                    Resumo = "Bruxo",
+                    Categoria = "Filme"
+                },
+                new Models.Post
+                {
+                    Titulo = "Harry Potter",
+                    Resumo = "Bruxo",
+                    Categoria = "Filme"
+                },
+                new Models.Post
+                {
+                    Titulo = "Harry Potter",
+                    Resumo = "Bruxo",
+                    Categoria = "Filme"
+                },
+            };
+
+            ViewBag.Posts = lista;
+
+            return View();
+        }
+    }
+}
