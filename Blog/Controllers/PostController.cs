c6ff3c4435360d0449978489f2070f0d20b983a7
Alexandre Castro <alexandre.castro@outlook.com>
2019-02-04 22:27:34 -0200

    Update Adicionar for HttpPost

--- Blog/Controllers/PostController.cs
+++ Blog/Controllers/PostController.cs
@@ -42,6 +42,7 @@ namespace Blog.Controllers
             return View();
         }
 
+        [HttpPost]
         public IActionResult Adiciona(Models.Post novo)
         {
             Lista.Add(novo);
