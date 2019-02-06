d829fbbf3913e64531ea7c0891bc70f97ca34895
Alexandre Castro <alexandre.castro@outlook.com>
2019-02-06 20:30:21 -0200

    Logging SQL Query - dotnet run on project folder for Console

--- Blog/Infra/BlogContext.cs
+++ Blog/Infra/BlogContext.cs
@@ -1,21 +1,29 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using System;
 using System.IO;
 
 namespace Blog.Infra
 {
     public class BlogContext : DbContext
     {
+        public static readonly LoggerFactory MyLoggerFactory = new LoggerFactory(new[] { new ConsoleLoggerProvider((_, __) => true, true) });
+
         public DbSet<Models.Post> Posts { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            Console.WriteLine("teste");
+
             var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
                                                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                                                    .AddEnvironmentVariables();
             var configuration = builder.Build();
 
-            optionsBuilder.UseSqlServer(configuration.GetConnectionString("Blog"));
+            optionsBuilder.UseLoggerFactory(MyLoggerFactory)
+                          .UseSqlServer(configuration.GetConnectionString("Blog"));
         }
     }
 }
