b351f081822fdb71fa5e2712eae203c272e6bc20
Alexandre Castro <alexandre.castro@outlook.com>
2019-02-08 20:02:15 -0200

    Replaced html with HtmlHelper in New and Update forms

--- Blog/Models/Post.cs
+++ Blog/Models/Post.cs
@@ -10,14 +10,19 @@ namespace Blog.Models
     public class Post
     {
         public int Id { get; set; }
+
         [Required(ErrorMessage = "Título é obrigatório")]
         [StringLength(40)]
         public string Titulo { get; set; }
+
         [Required(ErrorMessage = "Resumo é obrigatório")]
         public string Resumo { get; set; }
+
         [RequiredIf("Resumo", "", ErrorMessage = "Categoria Obrigatória quando resumo preenchido")]
         public string Categoria { get; set; }
+
         public DateTime? DataPublicacao { get; set; }
+
         public bool Publicado { get; set; }
     }
 }
