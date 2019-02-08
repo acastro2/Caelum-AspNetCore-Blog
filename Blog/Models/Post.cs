9b1fc6b53c09cbce34459f54a182d77171e549ec
Alexandre Castro <alexandre.castro@outlook.com>
2019-02-07 22:32:50 -0200

    Add lab for validating class

--- Blog/Models/Post.cs
+++ Blog/Models/Post.cs
@@ -6,6 +6,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace Blog.Models
 {
     [Table("Post")]
+    [ValidateClass]
     public class Post
     {
         public int Id { get; set; }
---
+++ Blog/ValidationAttributes/ValidateClassAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Blog.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blog.ValidationAttributes
+{
+    public class ValidateClassAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var post = (Post)value;
+
+            //if (!string.IsNullOrWhiteSpace(post.Titulo) && !string.IsNullOrWhiteSpace(post.Categoria))
+            //{
+            //    return new ValidationResult("Titulo e Categoria não podem estar preenchidos", new[] { "Titulo", "Categoria" });
+            //}
+
+            return ValidationResult.Success;
+        }
+    }
+}
