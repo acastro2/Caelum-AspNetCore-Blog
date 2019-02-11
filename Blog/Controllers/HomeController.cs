8bb8f22c91a84ee8c52cbcf3b3297a979d333f03
Alexandre Castro <alexandre.castro@outlook.com>
2019-02-11 20:00:20 -0200

    Add home with boostrap layout, header, footer and aside partials

---
+++ Blog/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class HomeController : Controller
+    {
+        private DAO.PostDAO Dao;
+
+        public HomeController()
+        {
+            Dao = new DAO.PostDAO();
+        }
+
+        public IActionResult Index()
+        {
+            return View(Dao.ListaPublicados());
+        }
+    }
+}
