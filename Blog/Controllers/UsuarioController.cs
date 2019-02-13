855eb091ca5f9c75b1858705d52c5bb8bb7a9e3e
Alexandre Castro <alexandre.castro@outlook.com>
2019-02-12 22:46:02 -0200

    Add login page

---
+++ Blog/Controllers/UsuarioController.cs
@@ -0,0 +1,25 @@
+﻿using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Autentica(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Post", new { area = "Admin" });
+            }
+
+            return View("Login", model);
+        }
+    }
+}
