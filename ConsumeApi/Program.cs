49ee1f144ed4465bd8bcface97de4cf25352dd46
Alexandre Castro <alexandre.castro@outlook.com>
2019-02-15 22:56:01 -0200

    Fix

--- ConsumeApi/Program.cs
+++ ConsumeApi/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Blog.Models;
+using Newtonsoft.Json;
+using System;
 using System.Net.Http;
 
 namespace ConsumeApi
@@ -10,6 +12,15 @@ namespace ConsumeApi
             try
             {
                 const string url = "http://blogmls.azurewebsites.net/api/post/novo";
+
+                var p = new Post
+                {
+                    Titulo = "Teste (Oi 2)",
+                    Resumo = "Teste",
+                    Categoria = "Teste"
+                };
+
+                Console.WriteLine(JsonConvert.SerializeObject(u));
 
                 using (var client = new HttpClient())
                 {
