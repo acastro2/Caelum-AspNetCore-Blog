565107dfc41e2560b279bed6f04861aad6cfef0a
Alexandre Castro <alexandre.castro@outlook.com>
2019-02-15 22:54:38 -0200

    Add consome web api console for testing

---
+++ ConsumeApi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+
+namespace ConsumeApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                const string url = "http://blogmls.azurewebsites.net/api/post/novo";
+
+                using (var client = new HttpClient())
+                {
+                    var result = client.PostAsJsonAsync(url, p).Result;
+
+                    Console.WriteLine(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
